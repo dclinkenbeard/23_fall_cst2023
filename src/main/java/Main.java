0c6460ab0509f5d1ad44ea929fa773bbeaa307d6
dclinkenbeard <dclinkenbeard@csumb.edu>
2023-08-07 10:43:56 -0700

    Initial Commit Including the Google Style guide

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * This is a basic Class for demonstrating Java Syntax.
+ * <br>
+ * A Java class typically
+ * @author Dr.C (Drew A. Clinkenbear)
+ * @since 2023_08_07
+ */
+public class Main {
+
+}
