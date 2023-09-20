2b92e8a3497f0a4ecb1df5f5b355a51ea30b1b3c
DrC <dclinkenbeard@csumb.edu>
2023-09-20 11:40:14 -0700

    Person should be done.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,3 +1,20 @@
 public class Person {
+  String name;
+  Double power;
 
+  public Double getPower() {
+    return power;
+  }
+
+  public void setPower(Double power) {
+    this.power = power;
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+        "name='" + name + '\'' +
+        ", power=" + power +
+        '}';
+  }
 }
